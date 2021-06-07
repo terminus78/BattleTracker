bb77e94da9814b8b559cdce0d73b03659b00bb8f
Shawn Way <way864@gmail.com>
2021-06-07 18:30:32 -0400

    Testing solutions

--- entry/stack_question.py
+++ entry/stack_question.py
@@ -8,9 +8,12 @@ class Circle():
         self.map_frames = []
         for i in range(15):
             self.map_frames.append([])
+            self.root.rowconfigure(i, minsize=33)
             for j in range(15):
-                self.space = tk.Frame(master=self.root, relief='raised', borderwidth=1, bg='gray28')
-                self.space.grid(row=i, column=j)
+                self.root.columnconfigure(j, minsize=33)
+                space = tk.Frame(master=self.root, relief='raised', borderwidth=1, bg='gray28')
+                space.grid(row=i, column=j, sticky='nsew')
+                self.map_frames[i].append(space)
 
     def circle_points(self, radius):
         points = []
@@ -33,7 +36,7 @@ class Circle():
             points.extend(self.transform_ring_points(x, y))
         return points
 
-    def transform_ring_points(x, y):
+    def transform_ring_points(self, x, y):
         x = int(x)
         y = int(y)
         return [( x,  y),
@@ -55,5 +58,18 @@ class Circle():
         return offsets
 
     def highlight(self, radius):
-        start = [7, 7]
+        curr_pos = [7, 7]
+        points = self.circle_points(radius)
+        offsets = self.points_to_offsets(points)
         
+        for i in range(len(offsets)):
+            curr_pos[0] += offsets[i][0]
+            curr_pos[1] += offsets[i][1]
+            if curr_pos[0] < 15 and curr_pos[1] < 15 and curr_pos[0] >= 0 and curr_pos[1] >= 0:
+                self.map_frames[curr_pos[1]][curr_pos[0]].config(bg='SpringGreen3')
+
+if __name__ == '__main__':
+    window = tk.Tk()
+    circ = Circle(window)
+    circ.highlight(3)
+    window.mainloop()
